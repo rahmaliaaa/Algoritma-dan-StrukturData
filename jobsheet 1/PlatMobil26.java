42936538e84ce3228cdcc5e7a6fa5fa279d3bb56
rahmaliaaa <mozachikaaa@gmail.com>
2024-02-16 22:23:04 +0700

    update

--- jobsheet 1/PlatMobil26.java
+++ jobsheet 1/PlatMobil26.java
@@ -4,11 +4,19 @@ public class PlatMobil26 {
         Scanner sc = new Scanner(System.in);
 
         char[] kode = {'A', 'B', 'D', 'E', 'F', 'G', 'H', 'L', 'N', 'T'};
-        String [] kota = {"BANTEN", " JAKARTA", "BANDUNG", "CIREBON", "BOGOR", "PEKALONGAN", "SEMARANG", "SURABAYA", "MALANG", "TEGAL"};
+        char [][] kota = {{'B', 'A', 'N', 'T', 'E', 'N'},
+                        {'J', 'A', 'K', 'A', 'R', 'T', 'A'},
+                        {'B', 'A', 'N', 'D', 'U', 'N', 'G'},
+                        {'C', 'I', 'R', 'E', 'B', 'O', 'N'},
+                        {'B', 'O', 'G', 'O', 'R'},
+                        {'P', 'E', 'K', 'A', 'L', 'O', 'N', 'G', 'A', 'N'},
+                        {'S', 'E', 'M', 'A', 'R', 'A', 'N', 'G'},
+                        {'S', 'A', 'R', 'A', 'B', 'A', 'Y', 'A'},
+                        {'M', 'A', 'L', 'A', 'N', 'G'},
+                        {'T', 'E', 'G', 'A', 'L'}};
         char inputKode;
-
         do {
-            System.out.print("Masukkan kode plat mobil: ");
+            System.out.print("Masukkan lode plat mobil: ");
             inputKode = sc.next().charAt(0);
 
             int indexKode = -1;
@@ -18,10 +26,14 @@ public class PlatMobil26 {
                     break;
                 }
             }
-            if (indexKode == -1){
+            if (indexKode == 1){
                 System.out.println("Kode Plat Mobil tidak ditemukan!");
             } else {
-                System.out.println(" Nama Kota: " + kota[indexKode]);
+                System.out.print(" Nama Kota: ");
+                for (int j = 0; j< kota[indexKode].length; j++){
+                    System.out.print(kota[indexKode][j]);
+                }
+                System.out.println();
             }
             System.out.print("Ingin Memasukkan Kode Plat Mobil Lainnya ? (y/n) : ");
         } while (sc.next().charAt(0) == 'y');
