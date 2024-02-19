a8d8ecf473b76eb029b3c40d05f551279f24b1a8
rahmaliaaa <mozachikaaa@gmail.com>
2024-02-19 15:31:41 +0700

    update

--- Praktikum02/Sepeda.java
+++ Praktikum02/Sepeda.java
@@ -1,16 +1,24 @@
 public class Sepeda {
-    float kecepatan;
-    int gear;
+        float kecepatan;
+        int gear;
 
-    public void tambahKecepatan(float increment){
-        kecepatan += increment;
-    }
-    public void kurangiKecepatan(float decrement){
-        kecepatan -= decrement;
-    }
-    public void cetakInfo(){
-        System.out.println("Kecepatan : " + kecepatan);
-        System.out.println(" Gear : " + gear);
-        System.out.println("==================");
-    }
+        public void tambahKecepatan(float increment){
+            if (kecepatan + increment > 20) { // modifikasi 8
+                kecepatan = 20;
+            } else {
+                kecepatan += increment;
+            }
+        }
+        public void kurangiKecepatan(float decrement){
+            if (kecepatan - decrement < 0) {
+                kecepatan = 0; //modifikasi 9
+            } else {
+                kecepatan -= decrement;
+            }
+        }
+        public void cetakInfo(){
+            System.out.println("Kecepatan : " + kecepatan);
+            System.out.println(" Gear : " + gear);
+            System.out.println("==================");
+        }
 }
